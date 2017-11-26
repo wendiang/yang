63814d12dc9208a78fe536aeb829d043f25e176a
wendiang <371174590@qq.com>
2017-11-26 22:33:43 +0800

    add

--- js/index.js
+++ js/index.js
@@ -7,10 +7,20 @@ bg.style.width=window.innerWidth+"px"
    var li=document.querySelector(".body-left").getElementsByTagName("li");
     var right_box=document.querySelector(".right-box")
    for(var i=0;i<li.length;i++){
-   	li[i].onclick=function(e){
-   	    right_box.removeClass("right-box")
+   	li[i].onclick=function(){
+   		 //var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+   		 var db =openDatabase("mydb","1.0",'ok',2*1024*1024);
+   		 db.transaction(function (tx) {  
+   tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)');
+    tx.executeSql('INSERT INTO LOGS (id, log) VALUES (1, "菜鸟教程")');
+   tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "www.runoob.com")');
+
+})
    	}
    }
+  
+
   
 
 
